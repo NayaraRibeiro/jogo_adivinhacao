5c115577f7c369ad00523b68e65925ce33e3b67a
Nayara Ribeiro <nayararibeiro@gmail.com>
2016-12-28 15:05:10 -0200

    Code refactor

--- jogo_adivinhacao.rb
+++ jogo_adivinhacao.rb
@@ -1,30 +1,4 @@
-def boas_vindas
-
-    puts
-	puts "  .___.                                "
-	puts "  (O,O)           Bem-vindo ao         "
-	puts "  l)___)      Jogo da Adivinhação!     "
-	puts "  =o===o=                              "
-	puts "                                       "
-	
-    puts
-    puts "Qual é o seu nome?"
-    nome = gets.strip
-    puts "\n\n\n\n\n\n"
-    puts "Começaremos o jogo para você, #{nome}"
-    nome
-end
-
-def escolhe_dificuldade
-
-	puts "Qual nível de dificuldade deseja?"
-	puts "(1) - Fácil  (2) - Médio  (3) - Difícil  (4) - Muito Difícil  (5) - Impossível"
-	puts "Escolha: "
-	dificuldade = gets.to_i
-	return dificuldade
-
-end
-
+require_relative "ui"
 
 def escolhe_numero_secreto (dificuldade)
 
@@ -41,26 +15,11 @@ def escolhe_numero_secreto (dificuldade)
 		maximo = 200
 	end
 
-	puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+	mostra_numero_secreto_maximo maximo
 	numero_secreto = rand (maximo) + 1
-	puts "\n\n\n\n\n"
-	sleep 2
-	puts "Escolhido!!! Que tal adivinhar nosso numero secreto? Você terá 5 tentativas."
-	sleep 2
+	mostra_pergunta_inicio
+	
 	return numero_secreto
-end
-
-def entrar_tentativa_jogador (ultimas_tentativas, tentativa, limite_tentativas)
-
-	puts "\n"
-	puts "Tentativa número #{tentativa} de #{limite_tentativas}"
-	puts "Tentativas anteriores: #{ultimas_tentativas}"
-	puts "Entre com o numero: "
-	numero_entrada_jogador = gets.strip
-	puts "Será que acertou o número secreto?"
-	sleep 2
-	puts "Você chutou o número #{numero_entrada_jogador}" + "\n\n\n"
-	numero_entrada_jogador
 end
 	
 def verifica_se_acertou (numero_secreto, numero_entrada_jogador)
@@ -74,11 +33,9 @@ def verifica_se_acertou (numero_secreto, numero_entrada_jogador)
 	
 	maior = numero_secreto > numero_entrada_jogador.to_i
 	if maior
-		puts "Que pena... Você não acertou." + "\n" + "O numero secreto é maior."
-		sleep 1
+		mostra_nao_acertou_numero_secreto_maior
 	else
-		puts "Que pena... Você não acertou." + "\n"+ "O numero secreto é menor."
-		sleep 1
+		mostra_nao_acertou_numero_secreto_menor
 	end
 	return false
 end
@@ -86,7 +43,7 @@ end
 def jogar (dificuldade)
 	numero_secreto = escolhe_numero_secreto dificuldade
 	pontuacao_jogador = 1000
-	limite_tentativas = 5
+	limite_tentativas = 1
 	ultimas_tentativas = []
 
 	for tentativa in 1..limite_tentativas
@@ -101,56 +58,20 @@ def jogar (dificuldade)
 		
 	end
 
-puts "Sua pontuação foi #{pontuacao_jogador} pontos."
-puts "O numero secreto era #{numero_secreto}."
+	mostra_pontuacao_final pontuacao_jogador
+	mostra_numero_secreto numero_secreto
 
 end
 
-def ganhou
-    puts
-    puts "             OOOOOOOOOOO               "
-    puts "         OOOOOOOOOOOOOOOOOOO           "
-    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
-    puts "    OOOOOO      OOOOO      OOOOOO      "
-    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
-    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
-    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
-    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
-    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
-    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
-    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
-    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
-    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
-    puts "         OOOOOO         OOOOOO         "
-    puts "             OOOOOOOOOOOO0             "
-    puts
-    puts "      Parabéns!! Você acertou!         "
-    puts
-end
+def jogo_adivinhacao
 
+	boas_vindas
 
-def jogar_novamente
-
-	puts "Deseja jogar novamente? (S/N)"
-	quer_jogar = gets.strip
-	if quer_jogar.upcase == "S" || quer_jogar.upcase == "N"
-		quer_jogar.upcase == "N"	
-	else		
-		puts "Opção inválida. Tente novamente."
-		jogar_novamente
+	loop do
+		dificuldade = escolhe_dificuldade
+		jogar dificuldade
+		if jogar_novamente
+			break
+		end
 	end
 end
-
-
-boas_vindas
-
-loop do
-	dificuldade = escolhe_dificuldade
-	jogar dificuldade
-	if jogar_novamente
-		break
-	end
-end
-	
-
-
---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative "jogo_adivinhacao"
+
+	jogo_adivinhacao
